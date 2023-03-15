81694a9eefc62c8a62d00ee8daa62ac0fe381e5f
joalvarado2 <jonathanalva92@gmail.com>
2023-03-14 21:30:53 -0500

    component guitars and styles is added

--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,13 +1,44 @@
+import { useLoaderData } from "@remix-run/react";
+import Guitarra from "../components/guitarra";
 import { getGuitarras } from "../models/guitarras.server";
+import styles from "../styles/guitarras.css"
+
+export function meta() {
+  return {
+    title: 'GuitarLA - Tienda de Guitarras',
+    description: 'GuitarLA - Nuestra coleccion de guitarras'
+  }
+}
+
+export function links() {
+  return {
+    rel: 'stylesheet',
+    href: styles
+  }
+}
+
 
 export async function loader() {
   const guitarras = await getGuitarras();
-  console.log(guitarras)
-  return guitarras;
+  return guitarras.data;
 }
 
 const Tienda = () => {
-  return <div>tienda</div>;
+  const guitarras = useLoaderData();
+
+  return (
+    <main className="contenedor">
+      <h2 className="heading">Nuestra coleccion</h2>
+
+      {guitarras.length && (
+        <div className="guitarras-grid">
+          {guitarras.map((guitarra) => (
+            <Guitarra key={guitarra.id} guitarra={guitarra.attributes} />
+          ))}
+        </div>
+      )}
+    </main>
+  );
 };
 
 export default Tienda;
